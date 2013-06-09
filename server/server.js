132e3283c5d436b3ec0db4e5e87fb1c53fd45d93
alexandre <alexandre@pandaco.net>
2013-06-09 15:29:07 +0200

    Updated server with more complete response

--- server/server.js
+++ server/server.js
@@ -2,27 +2,58 @@ GameConfig = require('../js/config').loadGameConfig();
 
 var io = require('socket.io').listen(parseInt(GameConfig.port, 10));
 
-io.set('log level', 2);
+io.set('log level', 3);
 io.set('transports', ['websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
 
 
 var clients = {};
 
+var size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            ++size;
+        }
+    }
+    return size;
+};
+
+
 io.sockets.on('connection', function(socket) {
 
     socket.on('createParty', function(data) {
         socket.join(data.room);
-//        socket.set('username', data.username, function() {
-            clients[socket.id] = {'username': data.username, 'room': data.room, 'score': 0};
-            io.sockets.in(data.room).emit('serverResponse', {'type': 'createParty', 'message': data.username + ' has joined' + ' #' + data.room, 'count': io.sockets.clients(data.room).length/*, 'count': io.sockets.clients(data.room).length*/});
- //       });
+
+        // Room is empty, let's create a new one
+        if (size(clients[data.room]) === 0) {
+            clients[data.room] = [];
+        }
+
+        clients[data.room].push({'username': data.username, 'room': data.room, 'score': 0});
+
+        
+        clients[socket.id] = {'username': data.username, 'room': data.room, 'score': 0};
+        var response = {
+            'type':    'updateParty',
+            'message': data.username + ' has joined #' + data.room,
+            'count':   io.sockets.clients(data.room).length,
+            'users':   clients[data.room],
+            'debug':   ''
+        };
+        io.sockets.in(data.room).emit('serverResponse', response);
+
     });
 
     socket.on('joinParty', function(data) {
         socket.join(data.room);
-//        socket.set('username', data.username, function() {
-            clients[socket.id] = {'username': data.username, 'room': data.room, 'score': 0};
-            io.sockets.in(data.room).emit('serverResponse', {'type': 'joinParty', 'message': data.username + ' has joined' + ' #' + data.room, 'count': io.sockets.clients(data.room).length/*, 'count': io.sockets.clients(data.room).length*/});
-//        });
+        clients[data.room].push({'username': data.username, 'room': data.room, 'score': 0});
+        var response = {
+            'type':    'udpateParty',
+            'message': data.username + ' has joined #' + data.room,
+            'count':   io.sockets.clients(data.room).length,
+            'users':   clients[data.room],
+            'debug':   ''
+        };
+        io.sockets.in(data.room).emit('serverResponse', response);
     });
 });
