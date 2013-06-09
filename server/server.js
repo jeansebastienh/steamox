4bc27a37fb2f7b5b3d322d162b10f665f855ed59
alexandre <alexandre@pandaco.net>
2013-06-09 12:13:46 +0200

    serverResponse

--- server/server.js
+++ server/server.js
@@ -10,12 +10,12 @@ io.sockets.on('connection', function(socket) {
     socket.on('createParty', function(data) {
         socket.join(data.room);
         clients = io.sockets.clients(data.room)
-        io.sockets.in(data.room).emit('updateUserlist', {'count': clients.length});
+        io.sockets.in(data.room).emit('serverResponse', {'count': clients.length});
     });
 
     socket.on('joinParty', function(data) {
         socket.join(data.room);
         clients = io.sockets.clients(data.room)
-        io.sockets.in(data.room).emit('updateUserlist', {'count': clients.length});
+        io.sockets.in(data.room).emit('serverResponse', {'count': clients.length});
     });
 });
