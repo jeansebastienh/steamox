5d4a4cf242a9a6302c973ff50d7fc7278a93d9f3
alexandre <alexandre@pandaco.net>
2013-06-09 12:24:49 +0200

    Returned data from server after creating/joining rooms

--- server/server.js
+++ server/server.js
@@ -5,17 +5,24 @@ var io = require('socket.io').listen(parseInt(GameConfig.port, 10));
 io.set('log level', 2);
 io.set('transports', ['websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
 
+
+var clients = {};
+
 io.sockets.on('connection', function(socket) {
 
     socket.on('createParty', function(data) {
         socket.join(data.room);
-        clients = io.sockets.clients(data.room)
-        io.sockets.in(data.room).emit('serverResponse', {'count': clients.length});
+        socket.set('username', data.username, function() {
+            clients[socket.id] = {'username': data.username, 'room': data.room, 'score': 0};
+            io.sockets.in(data.room).emit('serverResponse', {'message': data.username + ' has joined' + '#' . data.room, 'userslist': io.sockets.clients(data.room), 'count': io.sockets.clients(data.room).length}});
+        });
     });
 
     socket.on('joinParty', function(data) {
         socket.join(data.room);
-        clients = io.sockets.clients(data.room)
-        io.sockets.in(data.room).emit('serverResponse', {'count': clients.length});
+        socket.set('username', data.username, function() {
+            clients[socket.id] = {'username': data.username, 'room': data.room, 'score': 0};
+            io.sockets.in(data.room).emit('serverResponse', {'message': data.username + ' has joined' + '#' . data.room, 'userslist': io.sockets.clients(data.room), 'count': io.sockets.clients(data.room).length}});
+        });
     });
 });
