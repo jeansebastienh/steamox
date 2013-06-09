0f120e65565cbe9080ca623131ef8d37f2d789f6
Jean-Sébastien Hedde <jeanseb@au-fil-du.net>
2013-06-09 12:13:22 +0200

    Client can create a party.

--- js/user.js
+++ js/user.js
@@ -11,3 +11,8 @@ User.prototype.joinParty = function(room)
     console.log('User "'+this.name+'" is trying to join "'+ room +'"');
     this.socket.emit('joinParty', {username: this.name, room: room});
 };
+User.prototype.createParty = function(room)
+{
+    console.log('User "'+this.name+'" is trying to create a party "'+ room +'"');
+    this.socket.emit('createParty', {username: this.name, room: room});
+};
