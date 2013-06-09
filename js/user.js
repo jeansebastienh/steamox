af19e494cf4ce988cce2d9df1177d704e0d76a1c
Jean-Sébastien Hedde <jeanseb@au-fil-du.net>
2013-06-09 15:18:21 +0200

    The waiting room lists clients.

--- js/user.js
+++ js/user.js
@@ -16,10 +16,24 @@ User.prototype.createParty = function(room)
     console.log('User "'+this.name+'" is trying to create a party "'+ room +'"');
     this.socket.emit('createParty', {username: this.name, room: room});
 };
-User.prototype.waitForResponse = function(room)
+User.prototype.waitForResponse = function(dom)
 {
+
+    var list = dom.getElementById('room-list-players');
+    var listItem = dom.createElement('li');
+    listItem.innerHTML = this.name;
+    list.appendChild(listItem);
+
     this.socket.on('serverResponse', function(response) {
         console.log('Server respond ' + response.message);
-        console.dir(response);
+        switch(response.type) {
+            case 'updateParty':
+                list.innerHTML = '';
+                for (var i = 0; i<= response.count; i++) {
+                    var listItem = dom.createElement('li');
+                    listItem.innerHTML = 'User i';
+                    list.appendChild(listItem);
+                }
+        }
     });
 };
